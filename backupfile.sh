0179f2d763457d752491b32477dae503ef8c8557
thanhha123 <thienhabka@gmail.com>
2014-11-27 16:46:53 +0700

    Update backupfile.sh

--- backupfile.sh
+++ backupfile.sh
@@ -7,18 +7,14 @@ while read line ; do
     index=$(($index+1))
 done < path.txt
 
-
 for i in "${MYARRAY[@]}";
 do
-
 arr=$(echo $i | tr "/" "_")
 
-for x in $arr
-do
+    for x in $arr
+    do
 
-tar -cf /root/backup/"$time""$x".tar $i
-
-
-done
+    tar -cf /root/backup/"$time""$x".tar $i
+    done
 done
 find $backup_dir -ctime 7 -type f -delete
