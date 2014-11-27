a63d8d9c0441044a7583e800d841bd361f8e20a6
thanhha123 <thienhabka@gmail.com>
2014-11-27 16:03:52 +0700

    Create backupfile.sh

---
+++ backupfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+time=`date +"%d%m%y-%H%M%S"`
+mkdir -p /root/backup
+index=0
+while read line ; do
+    MYARRAY[$index]="$line"
+    index=$(($index+1))
+done < path.txt
+
+
+for i in "${MYARRAY[@]}";
+do
+
+arr=$(echo $i | tr "/" "_")
+
+for x in $arr
+do
+
+tar -cf /root/backup/"$time""$x".tar $i
+
+
+done
+done
+find $backup_dir -ctime 7 -type f -delete
